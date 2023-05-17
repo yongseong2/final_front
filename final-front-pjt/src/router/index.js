86ccb8a24e5065074640d1fdde1cc8f26f61fa83
{yongseong2} <{ancjs369@naver.com}>
2023-05-18 08:11:55 +0900

    nav bar

--- final-front-pjt/src/router/index.js
+++ final-front-pjt/src/router/index.js
@@ -23,7 +23,7 @@ const routes = [
   },
   {
     path: '/movies',
-    name: 'MovieView',
+    name: 'MoviesView',
     component: MoviesView
   },
   {
--- final-front-pjt/src/views/TodayMovieView.vue
+++ final-front-pjt/src/views/TodayMovieView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>이건 TodayMovieView View</h1>
+    <h1>이건 TodayMovieView</h1>
   </div>
 </template>
 
